3ea7d26c35a4976a9ed3debc681b1f07799a742f
dogpoopy <103020425+dogpoopy@users.noreply.github.com>
2024-02-08 21:56:14 +0800

    miuicamera: Clean up duplicate flags

--- products/board.mk
+++ products/board.mk
@@ -4,10 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-BUILD_BROKEN_DUP_RULES := true
-
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-
 BOARD_VENDOR_SEPOLICY_DIRS += vendor/xiaomi/vayu-miuicamera/sepolicy/vendor
 
 include vendor/xiaomi/vayu-miuicamera/common/BoardConfigVendor.mk
